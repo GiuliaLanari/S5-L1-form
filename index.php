45cf66d6f5906759d58614272eff4f9fff91e59d
GiuliaLanari <giulia.lanari96@gmail.com>
2024-05-09 19:56:41 +0200

    add style

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
 </head>
-<body>
-    <div class="container">
+<body class="bg-black text-white">
+    <div class="container ">
     <h1 class="text-center my-5">Create a form to Upload your CSV</h1>
         <div class="row">
        
@@ -20,13 +20,13 @@
                 <label for="formFile" class="form-label">File CSV</label>
                 <input class="form-control" type="file" id="formFile" name="uploaded_file">
             </div>
-            <button type="submit" class="btn btn-primary">Load</button>
+            <button type="submit" class="btn btn-outline-success">Load</button>
         </form>
             </div>
         </div>
-        <div class="col-12 col-md-6 mx-auto my-4">
-        <h2>Download the export in CSV</h2>
-        <a class="btn btn-success" href="files/export.csv" download>Download</a>
+        <div class="col-12 col-md-10 mx-auto my-4 d-flex justify-content-center align-items-center">
+        <h2 class="me-2">Download the export in CSV</h2>
+        <a class="btn btn-outline-success" href="files/export.csv" download>Download</a>
         </div>
     </div>
 </body>
