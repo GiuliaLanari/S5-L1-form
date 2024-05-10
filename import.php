235c625f526c9904792af601ba23bb47c95f6e40
GiuliaLanari <giulia.lanari96@gmail.com>
2024-05-10 14:02:28 +0200

    add limiti

--- import.php
+++ import.php
@@ -2,7 +2,10 @@
 
 include __DIR__ . "/db.php";
 
-$file_name= "files/import.csv";
+
+if (isset($_FILES["uploaded_file"]) && $_FILES["uploaded_file"]["type"] === 'text/csv' && $_FILES["uploaded_file"]["size"] < 360) {
+
+$file_name=$_FILES["uploaded_file"]["tmp_name"];
 
 if($file_handle = fopen($file_name, "r")){
     fgetcsv($file_handle);
@@ -12,8 +15,34 @@ if($file_handle = fopen($file_name, "r")){
             "name"=> $data[1],
             "surname"=> $data[2],
             "email"=> $data[3],
-            "age"=> $data[4],
+            "age"=> $data[4]?: null,
         ]);
     }
     fclose($file_handle);
 }
+} else {
+    echo "File CVS non caricato";
+}
+
+$uploads_dir = 'uploads/';
+$extension = strtolower(pathinfo($_FILES["profile_image"]["name"], PATHINFO_EXTENSION));
+if (isset($_FILES["profile_image"]) && explode('/', $_FILES["profile_image"]["type"])[0] === 'image' && $_FILES["profile_image"]["size"] < 1024 * 1024) {
+    $new_name = rand(100000, 9999999) . '.' . $extension;
+    // $new_name = '1393922.png';
+    $dir_list = scandir($uploads_dir);
+    while (true) {
+        // verificare se il nome nuovo con l'estensione esiste già
+        // se non esiste già usalo
+        // altrimenti creare uno nuovo
+        if (!in_array($new_name, $dir_list)) break;
+        $new_name = rand(100000, 9999999) . '.' . $extension;
+    }
+
+    // copiare l'immagine nella cartella delle immagini
+    move_uploaded_file($_FILES["profile_image"]["tmp_name"], $uploads_dir . $new_name);
+
+    // caricare nel database il nome dell'immagine     
+} else {
+    echo 'Immagine non caricata';
+}
+
